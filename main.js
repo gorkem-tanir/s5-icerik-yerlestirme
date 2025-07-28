e528720b035021b62277658a8feaeb1393d71613
gorkem-tanir <tanirgorkem@gmail.com>
2025-07-28 16:26:57 +0300

    src value of cta image is set.

--- main.js
+++ main.js
@@ -14,3 +14,5 @@ for (let i = 0; i < navLinks.length; i++) {
 document.querySelector(".cta h1").textContent = siteContent.cta.h1;
 
 document.querySelector(".cta button").textContent = siteContent.cta.button;
+
+document.querySelector(".cta img").src = siteContent.images["cta-img"];
